1fb2c3151ad4cc48dde4a7f775b583d7cd4a4158
Bianca Trovo <btrovo@is154288.intra.cea.fr>
2019-03-19 17:39:45 +0100

    Changes for plot, indexing, finding minimum peak

--- Timelimit09_spatialfilter.m
+++ Timelimit09_spatialfilter.m
@@ -26,6 +26,8 @@ BT_setpath
 % choose subj & go to the right folder
 BT_getsubj
 
+clear answer LevelAnalysis name numlines prompt subj_folders
+
 %% Load preprocessed files
 
 if subjnum== 1 || subjnum== 18 || subjnum== 19 || subjnum== 20 || subjnum== 21 || subjnum== 22
@@ -37,46 +39,52 @@ end
 %% Clean data more and prepare for Spatial Filter
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Setting up indexes for getting only the good trials
+
+[idx_goodxcond,idx_goodtrls, idx_allbadtrls]= BTmy_cleandatamore(TRIALS);
+
 good_trls = setdiff([1:length(DATA_REJ_INTERP.trial)],idx_allbadtrls);
-isequal(idx_goodtrls',good_trls)
+
+if isequal(idx_goodtrls',good_trls)==1; disp('YES'); else disp('NO'); end;
+
 % redundant but we redo it just in case
 cond= [TRIALS.cond]; %we put all the conditions in a row
 cond(cond==32) = Inf;
 un_conds = unique(cond);
 newcond= cond(good_trls);
 
+clear cond un_conds 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 cfg=[];
 cfg.trials = good_trls;
 % cfg.demean = 'yes';
-cfg.preproc.lpfilter='yes';
-cfg.preproc.lpfreq= 30; %notch filter 50
+cfg.lpfilter='yes';
+cfg.lpfreq= 30; %notch filter 50
     DATA_REJ_INTERP= ft_preprocessing(cfg,DATA_REJ_INTERP);
-    
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % per condition 
-DATA_cond=[];
-for condi = 1:length(un_conds)
-    
-    cfg=[];
-    cfg.trials= find(newcond == un_conds(condi));
-    DATA_cond{condi} = ft_preprocessing(cfg,DATA_REJ_INTERP);
-
-end
+%     DATA_cond=[];
+%     for condi = 1:length(un_conds)
+% 
+%         cfg=[];
+%         cfg.trials= find(newcond == un_conds(condi));
+%         DATA_cond{condi} = ft_preprocessing(cfg,DATA_REJ_INTERP);
+% 
+%     end
 
 %% Do Spatial Filter here
 
 % Define parameters here (sampling Rate, Timelock, indexes condition)
 SR=500;
 t0= 3;
-Ind2 = [TRIALS.cond]' == 2;
-Ind4 = [TRIALS.cond]' == 4;
-Ind8 = [TRIALS.cond]' == 8;
-Ind16 = [TRIALS.cond]' == 16;
-Ind32 = [TRIALS.cond]' == 32;
+% Ind2 = [TRIALS.cond]' == 2;
+% Ind4 = [TRIALS.cond]' == 4;
+% Ind8 = [TRIALS.cond]' == 8;
+% Ind16 = [TRIALS.cond]' == 16;
+% Ind32 = [TRIALS.cond]' == 32;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -87,21 +95,20 @@ DATA_CUBE = myft_ftStruct2dataCube(DATA_REJ_INTERP);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Per condition
-DATA_CUBEcond=[];
-for condi = 1:length(un_conds)
-    
-    DATA_CUBEcond{condi} = myft_ftStruct2dataCube(DATA_cond{condi});
-    
-end
+% DATA_CUBEcond=[];
+% for condi = 1:length(un_conds)
+%     
+%     DATA_CUBEcond{condi} = myft_ftStruct2dataCube(DATA_cond{condi});
+%     
+% end
 
 %% Visualization of ROI around Cz before applying SF
 
-
+cfg=[];
 gavg = ft_timelockanalysis([],DATA_REJ_INTERP);
+cfg=[];
 cfg.layout = 'eeg_64_NM20884N.lay';
-% % cfg=[];
-% cfg.preproc.lpfilter='yes';
-% cfg.preproc.lpfreq= 50;
+% already filtered at 30Hz
 figure
 ft_multiplotER(cfg,gavg);
 
@@ -111,14 +118,14 @@ ft_multiplotER(cfg,gavg);
     clustavg= mean(TrialAvg(elec_clust,:));
 
 % Per condition
-    TrialAvg= mean(DATA_CUBEcond{5},3);
-    elec_clust= [20,21,29,30,31,39,40]; % cluster of 7 channels selected around Cz
-    clustavg= mean(TrialAvg(elec_clust,:));
+%     TrialAvg= mean(DATA_CUBEcond{5},3);
+%     elec_clust= [20,21,29,30,31,39,40]; % cluster of 7 channels selected around Cz
+%     clustavg= mean(TrialAvg(elec_clust,:));
 
 %% Plot to see if there is a nice RP
 
 tAx = [0:2000]./SR - 3;
-figure; H=plot(tAx, clustavg);
+g=figure; plot(tAx, clustavg);
 xlabel('Time (s)');
 ylabel('mean Amplitude (\muV)');
 title(['Subj ' num2str(subjnum) ', avg channels 20,21,29,30,31,39,40']);
@@ -127,25 +134,21 @@ legend('All conditions', 'Location','NorthWest');
 
 %  save figure for further comparisons
 filename= [sprintf('Readiness_Potential_subj%02d', subjnum) '.png'];
-% cd(parent_folder)
-% cd(results_Path);
-saveas(H,filename);
+cd(figures_Path);
+saveas(g,filename);
 
-%% ERROR TOPOPLOT
+%% TOPOPLOT
 
-% cfg = [];                            
-% % cfg.xlim = [0.3 0.5];                
-% % cfg.zlim = [0 6e-14];                
-% cfg.layout = 'eeg_64_NM20884N.lay';            
-% cfg.parameter = 'individual'; % the default 'avg' is not present in the data
-% figure; ft_topoplotER(cfg,clustavg'); colorbar
+cfg = [];                            
+cfg.xlim = [-1 -0.2];                               
+cfg.layout = 'eeg_64_NM20884N.lay';            
+h=figure; ft_topoplotER(cfg,gavg); colorbar
+title(['Subj ' num2str(subjnum) ', Topography RP']);
 
-% cfg.layout = 'eeg_64_NM20884N.lay';
-% figure; topoplot(cfg,clustavg');
-% title(['Subj ' num2str(subjnum) ', Topography RP avg channels 20,21,29,30,31,39,40']);
-% filename3= [sprintf('Readiness_Potential_topography_subj%02d', subjnum) '.png'];
-% cd(parent_folder)
-% saveas(k,filename3)
+%  save figure for further comparisons
+filename= [sprintf('Readiness_Potential_topography_subj%02d', subjnum) '.png'];
+cd(figures_Path);
+saveas(h,filename);
 
 %% Define window for Spatial Filter based on the peak amplitude (CRITICAL PART)
 
@@ -157,9 +160,17 @@ cfg = [];
 cfg.latency = [-0.2 0.2];
 Winavg = ft_selectdata(cfg,gavg);
 
+elec_clust= [20,21,29,30,31,39,40];
+selclustavg= mean(Winavg.avg(elec_clust,:));
 % Find minimum 
-[Amp,Lat]= min(Winavg.avg);
-Peak_Lat = Winavg.time(Lat)
+[Amp,Lat]= min(selclustavg);
+Peak_Lat = Winavg.time(Lat);
+
+l=figure; plot(Winavg.time, selclustavg);
+% click with cursor first 
+filename= [sprintf('subj%02d_peakLat', subjnum) '.png'];
+cd(figures_Path);
+saveas(l,filename);
 
 Peak_Win= [Peak_Lat-0.05 Peak_Lat+0]; %[Peak_Lat-0.05 Peak_Lat+0.05];
 
@@ -178,7 +189,7 @@ else
 end
 
 %% SPATIAL FILTER here
-
+% We apply on all the conditions to be fair 
 nTrials = size(DATA_CUBE,3);
 IndAll = true(nTrials,1);
 [trl,msf] = ems_ncond(DATA_CUBE,IndAll,[], @spf_basic_2time_diff,tWin,t0,SR);
@@ -204,42 +215,54 @@ SF_timecourses_bl = baseline_correct(trl,SR,3,blRange);
 
 %% Visualization of the time course after the Spatial Filter
 
-figure; k=plot(tAx,mean(SF_timecourses_bl))
-xlabel('Time (s)')
-ylabel('mean Amplitude (\muV)')
-title(['Subj ' num2str(subjnum) ', Spatial filter, baseline [ ' num2str(Peak_Win) ' ]'])
-legend('All conditions', 'Location','NorthWest')
+g=figure; plot(tAx,mean(SF_timecourses_bl));
+xlabel('Time (s)');
+ylabel('mean Amplitude (\muV)');
+title(['Subj ' num2str(subjnum) ', Spatial filter, baseline [ ' num2str(Peak_Win) ' ]']);
+legend('All conditions', 'Location','NorthWest');
 
 % save figure for further comparisons
 filename= [sprintf('Spatial_filter_subj%02d', subjnum) '.png'];
-cd(parent_folder)
-saveas(k,filename)
+cd(figures_Path);
+saveas(g,filename);
 
 %% Visualization of the topography after the Spatial Filter
 
 meanSF = mean(msf,2);
 cfg.layout = 'eeg_64_NM20884N.lay';
-j=figure;topoplot(cfg,meanSF);
+h=figure;topoplot(cfg,meanSF);
 title(['Subj ' num2str(subjnum) ', Topography Spatial filter, baseline [ ' num2str(Peak_Win) ' ]']);
 colorbar
 
 % save figure for further comparisons
 filename= [sprintf('Spatial_filter_topography_subj%02d', subjnum) '.png'];
-cd(parent_folder)
-saveas(j,filename)
+cd(figures_Path);
+saveas(h,filename);
 
-%% Save results for each participants in proper folder 
+%% Save results for each participant in their own folder 
 
-if input('Save SPATIAL FILTER results? ')
-    timeseries_folder= [current_subj_folder,'/Timeseries'];
-    if ~exist([timeseries_folder, '/SpatialFilter']); mkdir([timeseries_folder, '/SpatialFilter']); end
-    cd(fullfile(timeseries_folder,'/SpatialFilter'));
+% if input('Save SPATIAL FILTER results? ')
+%     timeseries_folder= [current_subj_folder,'/Timeseries'];
+%     if ~exist([timeseries_folder, '/SpatialFilter']); mkdir([timeseries_folder, '/SpatialFilter']); end
+%     cd(fullfile(timeseries_folder,'/SpatialFilter'));
+% 
+%     save SF_results_newBl Ind* tWin nTrials trl msf blRange SF_timecourses_bl tAx subjnum
+% end
 
-    save SF_results_newBl Ind* tWin nTrials trl msf d blRange SF_timecourses_bl tAx subjnum
-end
+%% Save results of each participant in general folder
+
+spatialfilter_folder= [results_Path,'/SpatialFilter'];
+    if ~exist([results_Path, '/SpatialFilter']); mkdir([results_Path, '/SpatialFilter']); end
+cd(fullfile(spatialfilter_folder));
+
+filename= [sprintf('subj%02d_SF_results_newBl', subjnum)];
+save(filename, 'Ind*', 'tWin', 'nTrials', 'trl', 'msf', 'blRange', 'SF_timecourses_bl', 'tAx', 'subjnum');
+
 %% Tell me what I have done so far
-disp(['subj ' num2str(subjnum) ' done'])
+disp(['subj ' num2str(subjnum) ' done']);
 
-close all
+close all;
+
+clearvars;
 
 %% End of the script
